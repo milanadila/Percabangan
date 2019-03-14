3b084db896766a815275a7928b63c65207e21f91
Milan Adila <milanadila@Milan.local>
2019-03-14 09:40:09 +0700

    first commit

---
+++ src/com/dicoding/javafundamental/percabangan/ifthen/ganti.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.percabangan.ifthen;
+
+void ganti() {
+
+    if (isOn){
+        lampu.nyala();
+        }
+    else {
+        System.out.println("Kondisi tidak terpenuhi...");
+        }
+}
---
+++ src/com/dicoding/javafundamental/percabangan/ifthen/hitungIndeksPrestasi.java
@@ -0,0 +1,21 @@
+package com.dicoding.javafundamental.percabangan.ifthen;
+
+void hitungIndeksPrestasi() {
+            int nilaiUjian = 80;
+            char indeksPrestasi;
+
+            if (nilaiUjian >= 90) {
+                indeksPrestasi = 'A';
+            } else if (nilaiUjian >= 80) {
+                indeksPrestasi = 'B';
+            } else if (nilaiUjian >= 70) {
+                indeksPrestasi = 'C';
+            } else if (nilaiUjian >= 60) {
+                indeksPrestasi = 'D';
+            } else if (nilaiUjian >= 50) {
+                indeksPrestasi = 'E';
+            } else {
+                indeksPrestasi = 'F';
+            }
+            System.out.println("Nilai ujian akhir anda adalah" + indeksPrestasi);
+}
